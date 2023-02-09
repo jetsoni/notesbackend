cddba0abe6b9aedfb441387e09333d2267c6fe34
Samuli Laaksonen <samulilaaksonen1@gmail.com>
2023-02-09 10:48:23 +0200

    backend connected with database

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Täytyy ensin alustaa kokonaan uudelleen render erillisiin hakemistoihin toteutetuille treeni noteseille
-// require('dotenv').config()
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-// const mongoose = require('mongoose')
-// const Note = require('./models/note')
+const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -26,17 +26,7 @@ app.use(express.static('build'))
 let notes = [
     {
         id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
+        content: "This is from let notes",
         important: true
     }
 ]
@@ -45,8 +35,8 @@ let notes = [
 
 // const password = process.argv[2]
 
-// const url =
-//     `mongodb+srv://jetsoni:${password}@cluster0.rykthl1.mongodb.net/noteApp?retryWrites=true&w=majority`
+// const url =  // toimii kun laittaa alle oikean salasanan, nyt sitten seuraavaksi yhdistetään Mongoon. HUOM viimeisintä versiota ie commitoitu eikä pushattu
+//     `mongodb+srv://jetsoni:salasana@cluster0.rykthl1.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 // mongoose.set('strictQuery', false)
 // mongoose.connect(url)
@@ -71,10 +61,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-    /*Note.find({}).then(notes => {*/
-    res.json(notes)
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
 })
-// })
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -86,35 +76,39 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+        // date: new Date(),
+        // id: generateId(),
+    })
 
-    notes = notes.concat(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 
-    response.json(note)
+    // notes = notes.concat(note)
+    // response.json(note)
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
+    // const id = Number(request.params.id)
+    // const note = notes.find(note => note.id === id)
 
-    response.json(note)
+    // if (note) {
+    //     response.json(note)
+    // } else {
+    //     response.status(404).end()
+    // }
+
+    // response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -126,7 +120,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
